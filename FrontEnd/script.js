6d13f59682918be51c54c19c7ed72634675a7cff
Gurvan B <gbourgeron@gmail.com>
2024-01-11 14:50:41 +0100

    Add first project element of the api (no loop)

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,4 +2,24 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
+
 // Création des balises
+const projet = works[0]; 
+
+const imageElement = document.createElement("img");
+imageElement.src = projet.imageUrl;
+imageElement.alt = projet.title;
+
+const figCaptionElement = document.createElement("figcaption");
+figCaptionElement.innerText = projet.title;
+
+
+// Création de l'élément figure et rattachement d'img et figcaption
+const figureElement = document.createElement("figure");
+figureElement.appendChild(imageElement);
+figureElement.appendChild(figCaptionElement);
+
+// Rattachement de la balise figure au DOM
+const divGallery = document.querySelector(".gallery");
+
+divGallery.appendChild(figureElement);
