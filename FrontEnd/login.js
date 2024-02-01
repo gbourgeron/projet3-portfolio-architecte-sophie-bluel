7652bc3ca03d23f2cce34692c04ebe618396c0a0
Gurvan B <gbourgeron@gmail.com>
2024-02-01 13:31:49 +0100

    USER LOGIN function reshape

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,61 +1,61 @@
-// Fonction pour se loguer 
-function userLogin() {
+// Function to login
+async function userLogin() {
 
     const errorMessage = document.getElementById("error-login-msg");
 
     const loginForm = document.getElementById("login-form");
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Évite le rechargement de la page
+    loginForm.addEventListener("submit", async function(event) {
+        event.preventDefault(); // Prevent page reload
 
-        // Création de l'objet de l'user
-        const user = {
-            email: event.target.querySelector("[name=email]").value,
-            password: event.target.querySelector("[name=password]").value
-        }
 
-        // Création de la charge utile au format JSON
-        const chargeUtile = JSON.stringify(user);
+        try {
+            // Create the user object
+            const user = {
+                email: event.target.querySelector("[name=email]").value,
+                password: event.target.querySelector("[name=password]").value
+            }
 
-        // Appel de la fonction fetch avec toutes les informations
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: chargeUtile
-        })
+            // Create payload to JSON format
+            const chargeUtile = JSON.stringify(user);
+        
+            // Call the Api with fetch function
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: chargeUtile
+            })
 
-        .then(response => {
             if (!response.ok) {
                 throw new Error(`Erreur HTTP! Statut: ${response.status}`);
             }
             
-            // Extraction du token de la réponse
-            return response.json();
-        })
-        
-        .then(data => {
-            // Stockage du token localement
+            const data = await response.json();
+
+            // Store token in localStorage
             const token = data.token;
             localStorage.setItem('token', token);            
             
-            // Redirection sur index.html après un login réussi
+            // Redirect to index.html after success login
             window.location.href = "index.html";
-        })
 
-        .catch(error => {
-            console.error('Erreur lors de la requête fetch:', error);
+        } catch(error) {
             errorMessage.classList.remove("hidden");
-
+    
             if (error instanceof TypeError) {
-                // Message si connexion au serveur impossible
-                errorMessage.textContent = "Erreur de connexion au serveur, veuillez essayer ultérieurement."
-            } else {
-                // Message si identifiants incorrects
-                errorMessage.textContent = "Votre combinaison email/mot de passe est incorrecte."
+                // Message if server's connexion not ok
+                errorMessage.textContent = "Erreur de connexion au serveur, veuillez essayer ultérieurement.";
                 setTimeout(() => {
                     errorMessage.classList.add("hidden");
-                }, 2000);
+                }, 3000);
+            } else {
+                // Message if log users are wrong
+                errorMessage.textContent = "Votre combinaison email/mot de passe est incorrecte.";
+                setTimeout(() => {
+                    errorMessage.classList.add("hidden");
+                }, 3000);
             }
-        });
+
+        }
     });
 
 }
